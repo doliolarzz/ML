e88aa8e7b493abed9f2000c6a1c8ce41a3c36fef
Warit Prasartritha <doliolarzz@Warits-MacBook-Pro.local>
2017-09-15 09:34:17 +0700

    First Initialize

---
+++ ML/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ML
+//
+//  Created by Warit Prasartritha on 9/15/2560 BE.
+//  Copyright © 2560 Warit Prasartritha. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
